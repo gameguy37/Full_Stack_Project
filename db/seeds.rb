0ab37d6bed08d292c338bdfb7cdcdc04fd87b23a
itspanicky <nicky.li92@gmail.com>
2019-05-28 14:31:20 -0400

    second commit to user-auth

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+u1 = User.create(name: 'Tim Thompson', email: 'timthompson@gmail.com', password: 'password123')
+u2 = User.create(name: 'Jim Johnson', email: 'jimjohnson@gmail.com', password: 'password123')
+u3 = User.create(name: 'Bill B.', email: 'bill@hotmail.com', password: 'password123')
+u4 = User.create(name: 'Gary S.', email: 'garysquarepants@gmail.com', password: 'password123')
+u5 = User.create(name: 'Colonel Forbin', email: 'cactus555@gmail.com', password: 'password123')
+u6 = User.create(name: 'Tela', email: 'rebel@gamehendge.com', password: 'password123')
+u7 = User.create(name: 'Rutherford the Brave', email: 'head-knight@gamehendge.com', password: 'password123')
+u8 = User.create(name: 'Mr. Palmer', email: 'accountant@gamehendge.com', password: 'password123')
+u9 = User.create(name: 'Wilson', email: 'king_of_prussia@gamehendge.com', password: 'password123')
+u10 = User.create(name: 'The Sloth', email: 'hitman@gamehendge.com', password: 'password123')
